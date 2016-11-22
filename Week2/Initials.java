fa327b6e39fd79a745523f682d2ad558c452cc1f
jc348287 <alfredsusanto@my.jcu.edu.au>
2016-11-22 14:55:01 +0800

    Week 2 Question 7

---
+++ Week2/Initials.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jc348287 on 22/11/2016.
+ */
+import javax.swing.*;
+public class Initials
+{
+    String name;
+    public String getName(String a)
+    {
+        name = a;
+        return name;
+    }
+    public void setName(String n)
+    {
+        name = n;
+    }
+    
+}
