a5999a9a52b1b3b8d76f52e5bf6f5e80006c95ad
jc348287 <alfredsusanto@my.jcu.edu.au>
2016-12-06 14:30:14 +0800

    Week 4

--- Week4/CountByAnything.java
+++ Week4/CountByAnything.java
@@ -1,7 +1,14 @@
 /**
  * Created by jc348287 on 6/12/2016.
  */
+import java.util.*;
 public class CountByAnything
 {
+    public static void main (String[] args)
+    {
+        int count;
+        Scanner input = new Scanner(System.in);
+        
 
+    }
 }
--- Week4/CountByThree.java
+++ Week4/CountByThree.java
@@ -3,5 +3,15 @@
  */
 public class CountByThree
 {
-
+    public static void main (String[] args)
+    {
+        int count;
+        for(count = 3; count <=300; ++count)
+        {
+            if (count % 30 == 0)
+                System.out.println(count);
+            else
+                System.out.println(count + " ");
+        }
+    }
 }
