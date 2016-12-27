52d6ec44c602bb6524b9274c84d6a8a35629f74e
jc348287 <alfredsusanto@my.jcu.edu.au>
2016-12-27 14:51:03 +0800

    Week 7

---
+++ Week7/DebugTwelve4.java
@@ -0,0 +1,41 @@
+// An employee ID can't be more than 999
+// Keep executing until user enters four valid employee IDs
+// This program throws a FixDebugEmployeeIDException
+import javax.swing.*;
+public class DebugTwelve4
+{
+   public static void main(String[] args)
+   {
+      int x;
+      String inStr, outString = "";
+      final int MAX = 999;
+      int[] emp = new int[4];
+      for(x = 0; x < emp.length; ++x)
+      {
+         inStr = JOptionPane.showInputDialog(null, "Enter employee ID number");  
+         try
+         {
+            emp[x] = Integer.parseInt(inStr);
+            if(emp[x] > MAX)
+            {
+               throw new FixDebugEmployeeIDException("Number too high " + emp[x]);
+            }
+         }
+         catch(NumberFormatException error)
+         {	
+            --x;
+            JOptionPane.showMessageDialog(null, inStr + "\nNonnumeric ID");
+         }
+         catch(FixDebugEmployeeIDException error)
+         {	
+	        --x;
+            JOptionPane.showMessageDialog(null, error.getMessage());
+         }
+      }
+      for(x = 0; x < emp.length; ++x)
+      {
+         outString = outString + emp[x] + " ";
+      }
+      JOptionPane.showMessageDialog(null, "Four valid IDS are: " + outString);    
+   }
+}
